fcf052172bb1c73059def011eeb13b717ae1b067
Iroz0587609677 <142968789+Iroz0587609677@users.noreply.github.com>
2024-04-02 14:01:35 +0300

    Canceling the delete filters option

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -16,9 +16,10 @@ export default function Filter() {
     // Create an empty object to store selected filters
     const selectedFilters = {};
 
-    // Filter out unchecked checkboxes and unselected radio buttons
     Object.keys(filter).forEach((key) => {
-      if (filter[key] !== false) { // Check for non-false values (true or custom values)
+      if (key !== 'amount' && filter[key]) {
+        selectedFilters[key] = filter[key];
+      } else if (key === 'amount' && filter[key] !== 0) {
         selectedFilters[key] = filter[key];
       }
     });
@@ -27,20 +28,16 @@ export default function Filter() {
     const matchingObjects = searchResults.filter((obj2Item) =>
       Object.keys(selectedFilters).every((data1) => {
         if (data1 === "amount") {
+          console.log("amount");
           return obj2Item[data1] >= selectedFilters[data1]
             && obj2Item[data1] <= parseInt(selectedFilters[data1]) + 150;
-        } else if (data1 in obj2Item && obj2Item[data1] === selectedFilters[data1]) {
-          return true;
         }
-        return false;
+        return data1 in obj2Item && obj2Item[data1];
       })
     );
-
     // Update setFilterResults only if there are matching objects
     setFilterResults(matchingObjects.length > 0 ? matchingObjects : []);
   };
-
-
 
   return (
     <div className="h-[600px] overflow-auto touch-auto p-2 ">
@@ -54,13 +51,12 @@ export default function Filter() {
 
       <details className='m-4 border-b  border-slate-950'>
         <summary className='my-4 font-bold '>צפי אורחים</summary>
-        <RadioFilter name=" 200-350" setField={setFilter} field={filter} customKey="amount" amount="200" />
-        <RadioFilter name=" 350-500" setField={setFilter} field={filter} customKey="amount" amount="350" />
-        <RadioFilter name=" 500-650" setField={setFilter} field={filter} customKey="amount" amount="500" />
-        <RadioFilter name=" 650-800" setField={setFilter} field={filter} customKey="amount" amount="600" />
-        <RadioFilter name=" 800-950 " setField={setFilter} field={filter} customKey="amount" amount="800" />
+        <RadioFilter name="200-350" setField={setFilter} field={filter} customKey="amount" amount="200" />
+        <RadioFilter name="350-500" setField={setFilter} field={filter} customKey="amount" amount="350" />
+        <RadioFilter name="500-650" setField={setFilter} field={filter} customKey="amount" amount="500" />
+        <RadioFilter name="650-800" setField={setFilter} field={filter} customKey="amount" amount="600" />
+        <RadioFilter name="800-950" setField={setFilter} field={filter} customKey="amount" amount="800" />
       </details>
-
 
       <details className='m-4 border-b  border-slate-950'>
         <summary className='my-4 font-bold '>כשרות</summary>
--- client/src/components/compoInputs/RadioFilter.jsx
+++ client/src/components/compoInputs/RadioFilter.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+// import React from 'react'
 
-export default function RadioFilter(props) {
-    const { name, setField, field, customKey, amount } = props;
+// export default function RadioFilter(props) {
+//     const { name, setField, field, customKey, amount } = props;
+//     return (
+//         <div>
+//             <div className='my-4'>
+//                 <input id={customKey} name={customKey} type="radio" autoComplete={customKey}
+//                     onChange={() => setField({ ...field, [customKey]: amount })}
+//                 />
+//                 <label className="m-4 text-sm font-medium leading-6 text-gray-900"
+//                     htmlFor={customKey}> {name} </label>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+import React from 'react';
+
+const RadioFilter = ({ name, setField, field, customKey, amount }) => {
+    const handleChange = () => {
+        if (amount === '') {
+            setField({});
+        } else {
+            setField({ ...field, [customKey]: amount });
+        }
+    };
+
     return (
-        <div>
-            <div className='my-4'>
-                <input id={customKey} name={customKey} type="radio" autoComplete={customKey}
-                    onChange={() => setField({ ...field, [customKey]: amount })}
-                />
-                <label className="m-4 text-sm font-medium leading-6 text-gray-900"
-                    htmlFor={customKey}> {name} </label>
-            </div>
-
+        <div className='my-4'>
+            <input type="radio"
+                id={name} name={customKey} value={amount} checked={field[customKey] === amount} onChange={handleChange} />
+            <label className="m-4 text-sm font-medium leading-6 text-gray-900" htmlFor={name}>{name}</label>
         </div>
-    )
-}
+    );
+};
+
+export default RadioFilter;
