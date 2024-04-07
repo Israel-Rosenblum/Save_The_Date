dc2a43f15937040a36d970c937b4c79c5a02e64f
Iroz0587609677 <142968789+Iroz0587609677@users.noreply.github.com>
2024-04-07 17:10:35 +0300

    display to the respons fiktering

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -7,6 +7,8 @@ import DataContext from './context/DataContext'
 
 export default function Display() {
   const { selectedDates, searchResults, setSearchResults, filterResults } = useContext(DataContext)
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+  
   //מציג את כל האולמות שנבחרו לפי התאריך
   useEffect(() => {
     async function getDates() {
@@ -18,17 +20,28 @@ export default function Display() {
 
   return (
     <div className='mt-20  bg-white '>
-      <h1 className='text-3xl tracking-wide text-center  bg-white'>נמצאו&nbsp;{filterResults.length}&nbsp;תוצאות</h1>
-      <div className="flex  justify-around gap-4 p-2">
-        <div className="h-full  w-full max-w-60 hidden lg:block">
+      <div className="flex justify-around gap-4 p-2">
+        <div className="h-full w-full max-w-60 hidden lg:block">
           <Filter />
         </div>
         <div className="h-full bg-yellow-500 w-full ">
           <ListHalls />
         </div>
-        <div className="h-full  max-w-[450px] w-full hidden xl:block ">
+        <div className="h-full max-w-[450px] w-full hidden xl:block ">
           <Contact />
         </div>
+      </div>
+
+      <div className="block md:hidden">
+        <div className={`h-full w-full ${isFilterVisible ? '' : 'hidden'}`}>
+          {isFilterVisible ? <Filter />:<ListHalls />}
+        </div>
+        <button
+          onClick={() => setIsFilterVisible(!isFilterVisible)}
+          className="cursor-pointer"
+        >
+          סינונים
+        </button>
       </div>
 
 
